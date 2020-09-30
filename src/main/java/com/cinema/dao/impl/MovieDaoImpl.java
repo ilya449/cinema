ccf1aedc50d3f9ab537746feb1913db6c143177e
Ilya <ilya03449@gmail.com>
2020-09-30 18:34:58 +0300

    created HibernateUtil and hibernate.cfg.xml, fixed other files

--- src/main/java/com/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/cinema/dao/impl/MovieDaoImpl.java
@@ -3,17 +3,43 @@ package com.cinema.dao.impl;
 import com.cinema.dao.MovieDao;
 import com.cinema.lib.Dao;
 import com.cinema.model.Movie;
+import com.cinema.util.HibernateUtil;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cant insert movie entity: " + movie, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Movie> getAll() {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Movie> getAllMoviesQuery = session.createQuery("from Movie", Movie.class);
+            return getAllMoviesQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all movies from DB", e);
+        }
     }
 }
--- src/main/java/com/cinema/model/Movie.java
+++ src/main/java/com/cinema/model/Movie.java
@@ -1,5 +1,10 @@
 package com.cinema.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -8,7 +13,11 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "movies")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NonNull
     private String title;

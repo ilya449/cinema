10baec95e2f71f6ed8b9c5658597998ad4f418c1
Ilya <ilya03449@gmail.com>
2020-10-06 16:17:35 +0300

    created implementations for CinemaHallService and MovieSessionService. Also created dao's interfaces for this two Services

---
+++ src/main/java/com/cinema/dao/CinemaHallDao.java
@@ -0,0 +1,11 @@
+package com.cinema.dao;
+
+import com.cinema.model.CinemaHall;
+import java.util.List;
+
+public interface CinemaHallDao {
+
+    CinemaHall add(CinemaHall cinemaHall);
+
+    List<CinemaHall> getAll();
+}
--- src/main/java/com/cinema/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/com/cinema/service/impl/MovieSessionServiceImpl.java
@@ -1,19 +1,25 @@
 package com.cinema.service.impl;
 
+import com.cinema.dao.MovieSessionDao;
+import com.cinema.lib.Inject;
+import com.cinema.lib.Service;
 import com.cinema.model.MovieSession;
 import com.cinema.service.MovieSessionService;
-
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class MovieSessionServiceImpl implements MovieSessionService {
+    @Inject
+    private MovieSessionDao movieSessionDao;
+
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        return null;
+        return movieSessionDao.findSessions(movieId, date);
     }
 
     @Override
     public MovieSession add(MovieSession session) {
-        return null;
+        return movieSessionDao.add(session);
     }
 }

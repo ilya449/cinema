3249c1e58a8c7abc75969e11b100e5beb47e2720
Ilya <ilya03449@gmail.com>
2020-10-06 15:36:42 +0300

    added models CinemaHall and MovieSession

---
+++ src/main/java/com/cinema/model/MovieSession.java
@@ -0,0 +1,32 @@
+package com.cinema.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movie_sessions")
+public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @NonNull
+    private Movie movie;
+    @OneToOne
+    @NonNull
+    private CinemaHall cinemaHall;
+    @NonNull
+    private LocalDateTime showTime;
+}

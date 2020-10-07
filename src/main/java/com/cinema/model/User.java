67201b93f98398effe46b5bb241faaace022153f
Ilya <ilya03449@gmail.com>
2020-10-07 13:00:16 +0300

    created User model

---
+++ src/main/java/com/cinema/model/User.java
@@ -0,0 +1,33 @@
+package com.cinema.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @Column(unique = true)
+    private String email;
+    @NonNull
+    private String password;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private byte[] salt;
+}

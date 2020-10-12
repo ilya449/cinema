eb280ecb961e30a0e44f8ecefc872e4bf928c410
Ilya <ilya03449@gmail.com>
2020-10-12 11:25:26 +0300

    created Order model, order service and dao interfaces

---
+++ src/main/java/com/cinema/model/Order.java
@@ -0,0 +1,35 @@
+package com.cinema.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @OneToMany
+    private List<Ticket> tickets;
+    @NonNull
+    private LocalDateTime orderDate;
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}

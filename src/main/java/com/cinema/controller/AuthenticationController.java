375c6eb43035652adac509e63c145ece1778b814
Ilya <ilya03449@gmail.com>
2020-10-22 16:01:36 +0300

    implemented AuthenticationController

--- src/main/java/com/cinema/controller/AuthenticationController.java
+++ src/main/java/com/cinema/controller/AuthenticationController.java
@@ -1,4 +1,31 @@
 package com.cinema.controller;
 
+import com.cinema.exception.AuthenticationException;
+import com.cinema.model.dto.UserRegistrationRequestDto;
+import com.cinema.service.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/registration")
+    private RedirectView registerUser(@RequestBody UserRegistrationRequestDto dto){
+        authenticationService.register(dto.getEmail(), dto.getPassword());
+        return new RedirectView("/hello");
+    }
+
+    @PostMapping("/registration")
+    private RedirectView authenticateUser(@RequestBody UserRegistrationRequestDto dto)
+            throws AuthenticationException {
+        authenticationService.login(dto.getEmail(), dto.getPassword());
+        return new RedirectView("/hello");
+    }
 }
